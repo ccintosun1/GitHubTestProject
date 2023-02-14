8ddce6898ce6e9cce13236ae2d153f2ecb85e1d0
Cemil <ccintosun1@gmail.com>
2023-02-14 17:54:09 +0300

    added more hello feature

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -4,5 +4,8 @@ public class Hello {
 
         System.out.println("Hello");
         System.out.println("Developing Hello feature ");
+        System.out.println("Github Repo is already published to Github");
+        System.out.println("Doing more work");
+
     }
 }
