fe056bc4e3a8be2c9a7582514ba69ddd16350e7d
Cemil <ccintosun1@gmail.com>
2023-02-14 03:18:16 +0300

    started tracking .idea and iml folder

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -3,5 +3,13 @@ public class HelloTest {
     public static void main(String[] args) {
 
         System.out.println("HelloTest");
+
+        // work 1
+
+        // work 2
+
+        // work 3
+
+
     }
 }
